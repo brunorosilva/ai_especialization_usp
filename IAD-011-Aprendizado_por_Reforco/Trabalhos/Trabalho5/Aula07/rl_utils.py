3ee701883763f686bf756f29d77f1bbbb153c131
Bruno Rodrigues Silva <b.rosilva1@gmail.com>
2021-07-04 23:59:00 -0300

    T5 RL

---
+++ IAD-011-Aprendizado_por_Reforco/Trabalhos/Trabalho5/Aula07/rl_utils.py
@@ -0,0 +1,444 @@
+#!/usr/bin/env python
+
+"""An abstract class that specifies the Agent API for RL-Glue-py.
+"""
+
+from abc import ABCMeta, abstractmethod
+
+
+class BaseAgent:
+    """Implements the agent for an RL-Glue environment.
+    Note:
+        agent_init, agent_start, agent_step, agent_end, agent_cleanup, and
+        agent_message are required methods.
+    """
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def agent_init(self, agent_info= {}):
+        """Setup for the agent called when the experiment first starts."""
+
+    @abstractmethod
+    def agent_start(self, observation):
+        """The first method called when the experiment starts, called after
+        the environment starts.
+        Args:
+            observation (Numpy array): the state observation from the environment's evn_start function.
+        Returns:
+            The first action the agent takes.
+        """
+
+    @abstractmethod
+    def agent_step(self, reward, observation):
+        """A step taken by the agent.
+        Args:
+            reward (float): the reward received for taking the last action taken
+            observation (Numpy array): the state observation from the
+                environment's step based, where the agent ended up after the
+                last step
+        Returns:
+            The action the agent is taking.
+        """
+
+    @abstractmethod
+    def agent_end(self, reward):
+        """Run when the agent terminates.
+        Args:
+            reward (float): the reward the agent received for entering the terminal state.
+        """
+
+    @abstractmethod
+    def agent_cleanup(self):
+        """Cleanup done after the agent ends."""
+
+    @abstractmethod
+    def agent_message(self, message):
+        """A function used to pass information from the agent to the experiment.
+        Args:
+            message: The message passed to the agent.
+        Returns:
+            The response (or answer) to the message.
+        """
+
+"""Abstract environment base class for RL-Glue-py.
+"""
+
+class BaseEnvironment:
+    """Implements the environment for an RLGlue environment
+
+    Note:
+        env_init, env_start, env_step, env_cleanup, and env_message are required
+        methods.
+    """
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        reward = None
+        observation = None
+        termination = None
+        self.reward_obs_term = (reward, observation, termination)
+
+    @abstractmethod
+    def env_init(self, env_info={}):
+        """Setup for the environment called when the experiment first starts.
+
+        Note:
+            Initialize a tuple with the reward, first state observation, boolean
+            indicating if it's terminal.
+        """
+
+    @abstractmethod
+    def env_start(self):
+        """The first method called when the experiment starts, called before the
+        agent starts.
+
+        Returns:
+            The first state observation from the environment.
+        """
+
+    @abstractmethod
+    def env_step(self, action):
+        """A step taken by the environment.
+
+        Args:
+            action: The action taken by the agent
+
+        Returns:
+            (float, state, Boolean): a tuple of the reward, state observation,
+                and boolean indicating if it's terminal.
+        """
+
+    @abstractmethod
+    def env_cleanup(self):
+        """Cleanup done after the environment ends"""
+
+    @abstractmethod
+    def env_message(self, message):
+        """A message asking the environment for information
+
+        Args:
+            message: the message passed to the environment
+
+        Returns:
+            the response (or answer) to the message
+        """
+
+"""Glues together an experiment, agent, and environment.
+"""
+
+class RLGlue:
+    """RLGlue class
+
+    args:
+        env_name (string): the name of the module where the Environment class can be found
+        agent_name (string): the name of the module where the Agent class can be found
+    """
+
+    def __init__(self, env_class, agent_class):
+        self.environment = env_class()
+        self.agent = agent_class()
+
+        self.total_reward = None
+        self.last_action = None
+        self.num_steps = None
+        self.num_episodes = None
+
+    def rl_init(self, agent_init_info={}, env_init_info={}):
+        """Initial method called when RLGlue experiment is created"""
+        self.environment.env_init(env_init_info)
+        self.agent.agent_init(agent_init_info)
+
+        self.total_reward = 0.0
+        self.num_steps = 0
+        self.num_episodes = 0
+
+    def rl_start(self, agent_start_info={}, env_start_info={}):
+        """Starts RLGlue experiment
+
+        Returns:
+            tuple: (state, action)
+        """
+        
+        self.total_reward = 0.0
+        self.num_steps = 1
+
+        last_state = self.environment.env_start()
+        self.last_action = self.agent.agent_start(last_state)
+
+        observation = (last_state, self.last_action)
+
+        return observation
+
+    def rl_agent_start(self, observation):
+        """Starts the agent.
+
+        Args:
+            observation: The first observation from the environment
+
+        Returns:
+            The action taken by the agent.
+        """
+        return self.agent.agent_start(observation)
+
+    def rl_agent_step(self, reward, observation):
+        """Step taken by the agent
+
+        Args:
+            reward (float): the last reward the agent received for taking the
+                last action.
+            observation : the state observation the agent receives from the
+                environment.
+
+        Returns:
+            The action taken by the agent.
+        """
+        return self.agent.agent_step(reward, observation)
+
+    def rl_agent_end(self, reward):
+        """Run when the agent terminates
+
+        Args:
+            reward (float): the reward the agent received when terminating
+        """
+        self.agent.agent_end(reward)
+
+    def rl_env_start(self):
+        """Starts RL-Glue environment.
+
+        Returns:
+            (float, state, Boolean): reward, state observation, boolean
+                indicating termination
+        """
+        self.total_reward = 0.0
+        self.num_steps = 1
+
+        this_observation = self.environment.env_start()
+
+        return this_observation
+
+    def rl_env_step(self, action):
+        """Step taken by the environment based on action from agent
+
+        Args:
+            action: Action taken by agent.
+
+        Returns:
+            (float, state, Boolean): reward, state observation, boolean
+                indicating termination.
+        """
+        ro = self.environment.env_step(action)
+        (this_reward, _, terminal) = ro
+
+        self.total_reward += this_reward
+
+        if terminal:
+            self.num_episodes += 1
+        else:
+            self.num_steps += 1
+
+        return ro
+
+    def rl_step(self):
+        """Step taken by RLGlue, takes environment step and either step or
+            end by agent.
+
+        Returns:
+            (float, state, action, Boolean): reward, last state observation,
+                last action, boolean indicating termination
+        """
+
+        (reward, last_state, term) = self.environment.env_step(self.last_action)
+
+        self.total_reward += reward;
+
+        if term:
+            self.num_episodes += 1
+            self.agent.agent_end(reward)
+            roat = (reward, last_state, None, term)
+        else:
+            self.num_steps += 1
+            self.last_action = self.agent.agent_step(reward, last_state)
+            roat = (reward, last_state, self.last_action, term)
+
+        return roat
+
+    def rl_cleanup(self):
+        """Cleanup done at end of experiment."""
+        self.environment.env_cleanup()
+        self.agent.agent_cleanup()
+
+    def rl_agent_message(self, message):
+        """Message passed to communicate with agent during experiment
+
+        Args:
+            message: the message (or question) to send to the agent
+
+        Returns:
+            The message back (or answer) from the agent
+
+        """
+
+        return self.agent.agent_message(message)
+
+    def rl_env_message(self, message):
+        """Message passed to communicate with environment during experiment
+
+        Args:
+            message: the message (or question) to send to the environment
+
+        Returns:
+            The message back (or answer) from the environment
+
+        """
+        return self.environment.env_message(message)
+
+    def rl_episode(self, max_steps_this_episode):
+        """Runs an RLGlue episode
+
+        Args:
+            max_steps_this_episode (Int): the maximum steps for the experiment to run in an episode
+
+        Returns:
+            Boolean: if the episode should terminate
+        """
+        is_terminal = False
+
+        self.rl_start()
+
+        while (not is_terminal) and ((max_steps_this_episode == 0) or
+                                     (self.num_steps < max_steps_this_episode)):
+            rl_step_result = self.rl_step()
+            is_terminal = rl_step_result[3]
+
+        return is_terminal
+
+    def rl_return(self):
+        """The total reward
+
+        Returns:
+            float: the total reward
+        """
+        return self.total_reward
+
+    def rl_num_steps(self):
+        """The total number of steps taken
+
+        Returns:
+            Int: the total number of steps taken
+        """
+        return self.num_steps
+
+    def rl_num_episodes(self):
+        """The number of episodes
+
+        Returns
+            Int: the total number of episodes
+
+        """
+        return self.num_episodes
+
+
+import numpy as np
+from copy import deepcopy
+
+class Environment(BaseEnvironment):
+    """Implements the environment for an RLGlue environment
+
+    Note:
+        env_init, env_start, env_step, env_cleanup, and env_message are required
+        methods.
+    """
+
+    # def __init__(self):
+
+
+    def env_init(self, env_info={}):
+        """Setup for the environment called when the experiment first starts.
+
+        Note:
+            Initialize a tuple with the reward, first state observation, boolean
+            indicating if it's terminal.
+        """
+        self.rows = 4
+        self.cols = 12
+        self.start = [0,0]
+        self.goal = [0,11]
+        self.current_state = None
+
+    def env_start(self):
+        """The first method called when the episode starts, called before the
+        agent starts.
+
+        Returns:
+            The first state observation from the environment.
+        """
+        self.current_state = self.start  # An empty NumPy array
+
+        self.reward_obs_term = (0.0, self.observation(self.current_state), False)
+
+        return self.reward_obs_term[1]
+
+    def env_step(self, action):
+        """A step taken by the environment.
+
+        Args:
+            action: The action taken by the agent
+
+        Returns:
+            (float, state, Boolean): a tuple of the reward, state observation,
+                and boolean indicating if it's terminal.
+        """
+
+        new_state = deepcopy(self.current_state)
+
+        if action == 0: #right
+            new_state[1] = min(new_state[1]+1, self.cols-1)
+        elif action == 1: #down
+            new_state[0] = max(new_state[0]-1, 0)
+        elif action == 2: #left
+            new_state[1] = max(new_state[1]-1, 0)
+        elif action == 3: #up
+            new_state[0] = min(new_state[0]+1, self.rows-1)
+        else:
+            raise Exception("Invalid action.")
+        self.current_state = new_state
+
+        reward = -1.0
+        is_terminal = False
+        if self.current_state[0] == 0 and self.current_state[1] > 0:
+            if self.current_state[1] < self.cols - 1:
+                reward = -100.0
+                self.current_state = deepcopy(self.start)
+            else:
+                is_terminal = True
+
+        self.reward_obs_term = (reward, self.observation(self.current_state), is_terminal)
+
+        return self.reward_obs_term
+
+    def observation(self, state):
+        return state[0] * self.cols + state[1] 
+
+    def env_cleanup(self):
+        """Cleanup done after the environment ends"""
+        pass
+
+    def env_message(self, message):
+        """A message asking the environment for information
+
+        Args:
+            message (string): the message passed to the environment
+
+        Returns:
+            string: the response (or answer) to the message
+        """
+        if message == "what is the current reward?":
+            return "{}".format(self.reward_obs_term[0])
+
+        # else
+        return "I don't know how to respond to your message"
---
+++ IAD-012-Topicos_Avancados_em_Deep_Learning/Trabalhos/Trabalho4/descricao.md
@@ -0,0 +1,13 @@
+Nesse trabalho você deve  criar um autoencoder variacional com camadas convolucionais usando as imagens do conjunto "Flicker Faces HQ", que pode ser obtido em https://www.kaggle.com/arnaud58/flickrfaceshq-dataset-ffhq.
+
+Para facilitar o treinamento do autoencoder redimensione as imagens para 64x64x3, ou se for necessário para  32x32x3 pixels.
+
+Após treinar o seu autoencoder realize os seguintes testes:
+
+1) Reconstrução de imagens do conjunto de dados. Reconstrua pelo menos 16 imagens e faça um gráfico das imagens reconstruídas junto com as imagens originais. Além disso, calcule o erro de reconstrução de cada imagem.
+
+2) Criação de imagens novas originais por meio da combinação da representação latente de duas imagens. Nessa etapa crie várias transições entre duas imagens do conjunto de dados, iniciando de uma delas até obter a outra. Faça isso para pelo menos 8 pares de imagens do conjunto de dados.
+
+3) Criação de imagens novas originais a partir de vetores de ruídos. Criei pelo menos 16  imagens.
+
+Para entregar o seu trabalho, transforme o seu notebook em pdf e faça o upload do mesmo nesse espaço. 
